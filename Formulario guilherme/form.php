c5e18b9c100f94cf20e1940a158a8c6d2d1d5e25
guioliver <44738532+guioliver@users.noreply.github.com>
2018-11-26 20:33:23 -0200

    Update form.php

--- Formulario guilherme/form.php
+++ Formulario guilherme/form.php
@@ -7,7 +7,13 @@ $Sexo=$_POST["sexo"];
 
 echo 'Nome: ' ($_POST["nome"] ?? ''); "<br>";
 echo 'Endereço: ' ($_POST["endereço"] ?? ''); "<br>"
-echo 'Sexo: ' ($_POST["endereço"] ?? '');  "<br>"
+echo 'Sexo: ' ($_POST["sexo"] ?? '');  "<br>"
+ 
+ if (Email ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){ 
+					echo "</b>E-mail enviado com sucesso!</b>"; 
+					} 
+			else{ 
+					echo "</b>Falha no envio de E-Mail!</b>"; } 
  ;
  ;
 
